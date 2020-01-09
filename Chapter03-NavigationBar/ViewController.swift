62851777f7cfc7de2e4386af350f76b1dfd8494a
junseongday <junseongday@gmail.com>
2020-01-09 22:36:56 +0900

    네비개이션 바 스텝1

--- Chapter03-NavigationBar/ViewController.swift
+++ Chapter03-NavigationBar/ViewController.swift
@@ -13,8 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.initTitle()
     }
 
+    func initTitle() {
+        let nTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        nTitle.numberOfLines = 2
+        nTitle.textAlignment = .center
+        nTitle.font = UIFont.systemFont(ofSize: 15)
+        nTitle.text = "58개 숙소 \n 1박(1월 10일 ~ 1월 11일)"
+        self.navigationItem.titleView = nTitle
+        
+        let color = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
+        self.navigationController?.navigationBar.barTintColor = color
+    }
 
 }
 
